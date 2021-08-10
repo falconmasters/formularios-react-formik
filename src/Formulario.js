589b8e39d3f25c2848e8db66ed329b6de852cfda
Carlos <carlos@falconmasters.com>
2021-08-10 10:17:50 -0500

    Archivos iniciales

---
+++ src/Formulario.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+
+const Formulario = () => {
+	const [inputNombre, cambiarInputNombre] = useState('');
+	const [inputCorreo, cambiarInputCorreo] = useState('');
+
+	// Funcion que se encargara de validar los datos y enviar el formulario
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// Comprobamos validacion del formulario ...
+		// Si todo es correcto enviamos el formulario
+
+		console.log('Formulario Enviado!');
+	}
+
+	// Funcion que se encarga de cambiar el estado del inputNombre
+	const handleInputNombre = (e) => {
+		cambiarInputNombre(e.target.value);
+	}
+	
+	// Funcion que se encarga de cambiar el estado del inputCorreo
+	const handleInputCorreo = (e) => {
+		cambiarInputCorreo(e.target.value);
+	}
+
+	return (
+		<>
+			<form action="" onSubmit={handleSubmit} className="formulario">
+				<div>
+					<label htmlFor="nombre">Nombre</label>
+					<input
+						type="text"
+						name="nombre"
+						placeholder="Nombre"
+						id="nombre"
+						value={inputNombre}
+						onChange={handleInputNombre}
+					/>
+				</div>
+
+				<div>
+					<label htmlFor="correo">Correo</label>
+					<input
+						type="text"
+						name="correo"
+						placeholder="Correo"
+						id="correo"
+						value={inputCorreo}
+						onChange={handleInputCorreo}
+					/>
+				</div>
+
+				<button type="submit">Enviar</button>
+			</form>
+		</>
+	);
+}
+ 
+export default Formulario;
